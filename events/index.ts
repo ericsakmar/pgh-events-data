6d2a66cbbe4d61d1e51c9cdd1f8c71e403a45e58
Eric Sakmar <eric.sakmar@gmail.com>
2026-09-11 08:20:39 -0400

    retry

--- events/index.ts
+++ events/index.ts
@@ -1,18 +1,53 @@
-import * as smalls from "./sources/smalls.ts";
 import * as amw from "./sources/amw.ts";
 import * as arcade from "./sources/arcade.ts";
+import * as smalls from "./sources/smalls.ts";
+import { type Event } from "./event.ts";
+
+const POST_NODE_TYPE = `Event`;
+const MAX_RETRIES = 3;
+
+const getWithRetry = async (
+  url: string,
+  getEvents: () => Promise<Event[]>,
+  retries = 0,
+): Promise<Event[]> => {
+  try {
+    const events = await getEvents();
+
+    if (events.length === 0) {
+      console.warn(`no events found for ${url}`);
+    }
+
+    return events;
+  } catch (error) {
+    if (retries < MAX_RETRIES) {
+      console.warn(`retrying ${url} (${retries + 1}/${MAX_RETRIES})`);
+      const retry = await getWithRetry(url, getEvents, retries + 1);
+      return retry;
+    } else {
+      console.warn(`max retries exceeded for ${url}`);
+      console.warn(error);
+
+      return [];
+    }
+  }
+};
 
 export const getEvents = async () => {
-  // TODO retries and stuff
   // TODO filter things that hav already happened
 
-  const allSources = [smalls.getEvents, amw.getEvents];
-  const devSources = [arcade.getEvents];
+  const allSources = [arcade, smalls, amw];
+
+  const devSources = [arcade];
 
   const sources =
     process.env.NODE_ENV === "development" ? devSources : allSources;
 
-  const events = (await Promise.all(sources.map((source) => source()))).flat();
+  const events = (
+    await Promise.all(
+      sources.map((source) => getWithRetry(source.url, source.getEvents)),
+    )
+  ).flat();
 
   return events;
 };
